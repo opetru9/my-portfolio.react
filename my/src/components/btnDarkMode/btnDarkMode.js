1cc9fce350578e7a694eea4805b7d4507cc309fb
opetru9 <opetrik9@mail.ru>
2024-08-06 16:08:57 +0300

    small_updates_02

--- my/src/components/btnDarkMode/btnDarkMode.js
+++ my/src/components/btnDarkMode/btnDarkMode.js
@@ -9,9 +9,12 @@ import './btnDarkMode.css'
 const BtnDarkMode = () => {
 
     let [darkMode, setDarkMode] = useLocalStorage('darkMode', systemLightMode());
-
+    
+  // create a reference we can use for find the element with whis ref as atribute
     const btnRef = useRef(null)
-
+    
+  // actualizam valoarea printr-o functie de actualizare folosind valoarea curenta- 
+  // "currentValue" pe care ne-o ofera React
     const toggleDarkMode = () =>{
         setDarkMode((currentValue)=>{
             return currentValue === 'light' ? 'dark' : 'light';
@@ -19,7 +22,6 @@ const BtnDarkMode = () => {
     }
 
     useEffect(()=>{
-
        if(darkMode === 'dark'){
             document.body.classList.add('dark');
             btnRef.current.classList.add('dark-mode-btn--active')
@@ -28,13 +30,16 @@ const BtnDarkMode = () => {
             document.body.classList.remove("dark");
             btnRef.current.classList.remove("dark-mode-btn--active");
         }
-
     },[darkMode])
 
+  // dupa ce s-a montat componentul evennt Listenerul incepe sa astepte schimbari in color scheme
     useEffect(() => {
+    // se creeaza un obiecat MediaQuerryList cu preferinta mea de culoare (dark)
       window
         .matchMedia("(prefers-color-scheme: dark)")
+    // la care se ataseaza un eventListener (MediaQueryListEvent)
         .addEventListener("change", (event) => {
+    // event.matches e true daca color Scheme noastra e egala cu MediaQuerryList setat (dark)
           const newColorScheme = event.matches ? "dark" : "light";
           setDarkMode(newColorScheme);
         });
